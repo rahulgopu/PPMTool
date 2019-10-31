7495ac61a235d97028bd30f54b43a6899a15b97a
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-10-31 17:26:02 -0500

    Project Object, Repository, Controller & Object Validation

---
+++ PPMTool-Java/src/main/java/io/rahul/ppmtool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package io.rahul.ppmtool.services;
+
+import io.rahul.ppmtool.domain.Project;
+import io.rahul.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        return projectRepository.save(project);
+    }
+}
