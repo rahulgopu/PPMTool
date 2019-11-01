3eca3d52083bf37a5181b329b53e36bef268ba5a
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-11-01 16:41:30 -0500

    Setting up Dev Environment & First React Component

---
+++ ppmtool-react/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Rahul</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
