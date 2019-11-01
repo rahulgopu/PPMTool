5e54757529b4a27324e865434c359e83519ed814
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-10-31 21:32:29 -0500

    Find all Projects

--- PPMTool-Java/src/main/java/io/rahul/ppmtool/repositories/ProjectRepository.java
+++ PPMTool-Java/src/main/java/io/rahul/ppmtool/repositories/ProjectRepository.java
@@ -8,4 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
     Project findByProjectIdentifier(String projectIdentifier);
+
+    @Override
+    Iterable<Project> findAll();
 }
