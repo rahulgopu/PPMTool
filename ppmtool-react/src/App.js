bb4065870c66c3594ed2165219ffb2a79cd3711e
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-11-02 13:38:47 -0500

    React Router

--- ppmtool-react/src/App.js
+++ ppmtool-react/src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
--- ppmtool-react/src/components/Dashboard.js
+++ ppmtool-react/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ProjectItem from "./project/ProjectItem";
+import CreateProjectButton from "./project/CreateProjectButton";
 
 class Dashboard extends Component {
   render() {
@@ -11,9 +12,7 @@ class Dashboard extends Component {
             <div className="col-md-12">
               <h1 className="display-4 text-center">Projects</h1>
               <br />
-              <a href="ProjectForm.html" className="btn btn-lg btn-info">
-                Create a Project
-              </a>
+              <CreateProjectButton />
               <br />
               <hr />
               <ProjectItem />
