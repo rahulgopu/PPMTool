42cdea1dfb1cb2dda22e4741597e3469e2b889f4
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-11-03 15:04:13 -0600

    Create Redux Store

--- ppmtool-react/src/App.js
+++ ppmtool-react/src/App.js
@@ -4,16 +4,20 @@ import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
