467d1cc953b96eb422f297d2d6e6f04620d632f2
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-11-08 15:25:58 -0600

    Backlog & ProjectTask Entities

---
+++ PPMTool-Java/src/main/java/io/rahul/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package io.rahul.ppmtool.repositories;
+
+import io.rahul.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
