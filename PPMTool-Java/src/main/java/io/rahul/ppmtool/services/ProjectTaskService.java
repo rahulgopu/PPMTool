672ddd80697331742c9e66b93897dd9b74b0cfc4
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-11-09 16:15:05 -0600

    Create and Persist the Project Task(PT)

---
+++ PPMTool-Java/src/main/java/io/rahul/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,50 @@
+package io.rahul.ppmtool.services;
+
+import io.rahul.ppmtool.domain.Backlog;
+import io.rahul.ppmtool.domain.ProjectTask;
+import io.rahul.ppmtool.repositories.BacklogRepository;
+import io.rahul.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        //PTs to be added to a specific project, project!= null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
+        //set the BL to PT
+        projectTask.setBacklog(backlog);
+
+        //Project Sequence
+        Integer backlogSequence = backlog.getPTSequence();
+
+        //Update the BL sequence
+        backlogSequence++;
+        backlog.setPTSequence(backlogSequence);
+
+        //Add sequence to project task
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        //Initial priority when priority is null
+        if(projectTask.getPriority() == null) {
+            projectTask.setPriority(3);
+        }
+
+        //Initial status when status is null
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO-DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
