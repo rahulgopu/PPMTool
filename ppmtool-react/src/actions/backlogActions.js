4ba36b44f501a24ab3251eb17c9997c1fc6075a4
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-11-16 15:33:23 -0600

    Load ProjectTasks to the state

--- ppmtool-react/src/actions/backlogActions.js
+++ ppmtool-react/src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_BACKLOG } from "./types";
 
 export const addProjectTask = (
   backlog_id,
@@ -12,6 +12,21 @@ export const addProjectTask = (
     dispatch({
       type: GET_ERRORS,
       payload: {}
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
+};
+
+export const getBacklog = backlog_id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}`);
+    dispatch({
+      type: GET_BACKLOG,
+      payload: res.data
     });
   } catch (err) {
     dispatch({
