86f92c7bce2c1aac4f55079f93a312b46e8fd962
Rahul Reddy Gopu <rahulreddy.gopu96@gmail.com>
2019-11-17 21:30:35 -0600

    Delete Project Task

--- ppmtool-react/src/reducers/backlogReducer.js
+++ ppmtool-react/src/reducers/backlogReducer.js
@@ -25,8 +25,10 @@ export default (state = initialState, action) => {
 
     case DELETE_PROJECT_TASK:
       return {
-        ...state
-        //TO-DO
+        ...state,
+        project_tasks: state.project_tasks.filter(
+          project_task => project_task.projectSequence !== action.payload
+        )
       };
 
     default:
